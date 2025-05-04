c7ee4dd40c5613d226be87d20b0fe75823c135fb
ranaabdellatif <ranaabdellatif645@gmail.com>
2025-05-04 16:47:58 -0600

    go

--- server/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # load environment variables from .env
-
-class Config:
-    MONGO_URI = os.getenv("MONGO_URI")
-    SECRET_KEY = os.getenv("JWT_SECRET_KEY")
